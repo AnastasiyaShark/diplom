eebb094b8e31b2b552b0008ba3edd8de5e879fe9
AnastasiyaShark <a.sharkovskaya@yandex.ru>
2021-04-25 12:01:35 +0200

    всё работает + файлы могу удалить и переименовывать в папке

--- diplom/src/main/java/ru/netology/diplom/service/FilesStorageServiceImpl.java
+++ diplom/src/main/java/ru/netology/diplom/service/FilesStorageServiceImpl.java
@@ -5,13 +5,14 @@ import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import ru.netology.diplom.exeption.ErrorInputData;
-import ru.netology.diplom.model.File;
+import ru.netology.diplom.model.FileI;
 import ru.netology.diplom.model.ListResponse;
 import ru.netology.diplom.model.RequestNewName;
 import ru.netology.diplom.repository.FileRepository;
 
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
@@ -59,7 +60,7 @@ public class FilesStorageServiceImpl implements FilesStorageService {
     //компоновка и сохранение файла в бд
     public void linkingAndSavingFile(String originalName, String generatedName, String path,
                                      int size, String userLogin) {
-        File newFile = new File();
+        FileI newFile = new FileI();
         newFile.setOriginalName(originalName);
         newFile.setGeneratedName(generatedName);
         newFile.setPath(path);
@@ -75,9 +76,9 @@ public class FilesStorageServiceImpl implements FilesStorageService {
         String newAuthHeader = authHeader.replace("Bearer ", "");
 
         String userName = sessionService.getUserNameByToken(newAuthHeader);
-        List<File> allFile = fileRepository.findAll();
+        List<FileI> allFile = fileRepository.findAll();
         List<ListResponse> newList = new ArrayList<>();
-        for (File file : allFile) {
+        for (FileI file : allFile) {
             if (file.getUsersLogin().equals(userName)) {
                 newList.add(new ListResponse(file.getGeneratedName(), file.getSize()));
             }
@@ -88,28 +89,57 @@ public class FilesStorageServiceImpl implements FilesStorageService {
 
     @Override
     public void delete(String fileName) {
+
+        FileI fileI = fileRepository.findFileIByGeneratedName(fileName);
+
+        deleteFileFromFolder(fileI.getPath()+fileName);
         fileRepository.deleteFileByGeneratedName(fileName);
 
     }
 
 
-   @Override
-    public Optional<File> load(String fileName)  {
+    @Override
+    public Optional<FileI> load(String fileName)  {
 
-       return fileRepository.findFileByGeneratedName(fileName);
-   }
+        return fileRepository.findFileByGeneratedName(fileName);
+    }
 
 
     @Override
     public void changeFileName(String fileName, RequestNewName name) {
-        Optional<File> file = fileRepository.findFileByGeneratedName(fileName);
-        File newFile = file.get();
+        Optional<FileI> file = fileRepository.findFileByGeneratedName(fileName);
+        String oldPath = file.get().getPath()+file.get().getGeneratedName();
+
+        FileI newFile = file.get();
         newFile.setOriginalName(name.getFilename());
         newFile.setGeneratedName(date + name.getFilename());
         fileRepository.save(newFile);
+
+        String newPath = newFile.getPath()+newFile.getGeneratedName();
+        renameFileInFolder(oldPath,newPath);
+
         fileRepository.deleteFileByGeneratedName(fileName);
-newFile.getUsersLogin();
+
 
     }
-}
 
+    public void deleteFileFromFolder (String path){
+
+        File file = new File((path));
+        if (file.exists()){
+            file.delete();
+        }else {
+            throw new ErrorInputData("File " +  path + "does not exist");
+        }
+    }
+
+    public void renameFileInFolder (String oldPath,String newPath){
+
+        File file = new File(oldPath);
+        if (file.exists()){
+            file.renameTo(new File(newPath));
+        }else {
+            throw new ErrorInputData("File " +  oldPath + "does not exist");
+        }
+    }
+}
